2075cf41d8777bece77f8a678391718ba9a8385f
angkb <sgpkokbeng@yahoo.com.sg>
2024-03-15 16:36:54 +0800

    add dynamodb table for state locking

--- src/backend.tf
+++ src/backend.tf
@@ -3,5 +3,8 @@ terraform{
       bucket = "sctp-ce4-tfstate-bucket"
       key = "src-s3-CE4-Grp3-Capstone.tfstate"
       region = "ap-southeast-1"
+
+      # State Locking
+    dynamodb_table = "quintet-state"
     }
 }
