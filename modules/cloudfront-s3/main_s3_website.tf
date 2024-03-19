fcbe46364d4f285daf87c9336201e331aaf230a8
angkb <sgpkokbeng@yahoo.com.sg>
2024-03-19 09:33:24 +0800

    add s3_bucket_lifecycle_congfiguration block in main_s3_website.tf + update HISTORY.md

--- modules/cloudfront-s3/main_s3_website.tf
+++ modules/cloudfront-s3/main_s3_website.tf
@@ -8,6 +8,57 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true
 }
 
+# create lifecycle configuration :
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id = "log"
+
+    expiration {
+      days = 90
+    }
+
+    filter {
+      and {
+        prefix = "log/"
+
+        tags = {
+          rule      = "log"
+          autoclean = "true"
+        }
+      }
+    }
+
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+
+  rule {
+    id = "tmp"
+
+    filter {
+      prefix = "tmp/"
+    }
+
+    expiration {
+      date = "2024-09-30T00:00:00Z"
+    }
+
+    status = "Enabled"
+  }
+}
+
+
 # create s3 bucket access logging
 resource "aws_s3_bucket_logging" "access-log-bucket" {
    bucket = aws_s3_bucket.bucket.id
