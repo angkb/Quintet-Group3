fbace9d3bc06905c18e36df8cf801b56b760f106
angkb <sgpkokbeng@yahoo.com.sg>
2024-03-21 15:47:13 +0800

    retify checkov CKV_AWS_145 + update HISTORY.md

--- modules/cloudfront-s3/main.tf
+++ modules/cloudfront-s3/main.tf
@@ -8,11 +8,18 @@ resource "aws_s3_bucket" "static_web" {
 }
 
 # encrypt bucket using SSE-S3  -checkov-CKV_AWS_145
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
   bucket = aws_s3_bucket.static_web.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm = "aws:kms"
     }
   }
 }
